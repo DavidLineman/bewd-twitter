9d96da17bf171598ab1bc6dc2078f6a4b33c937b
DavidLineman <dline215@gmail.com>
2023-07-06 09:08:23 -0400

    tweet controller updates

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,34 @@
 class TweetsController < ApplicationController
+
+  def index 
+    @tweets = Tweet.all
+    render 'tweets/index'
+  end 
+
+  def create 
+    token = cookies.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      user = session.user
+      @tweet = user.tweets.new(tweet_params)
+
+      if @tweet.save
+        render 'tweets/create'
+      else 
+        render json: { success: false }
+      end 
+    else 
+      render json: { success: false }
+    end 
+  end
+
+  # need to make the jbuilder create template. That is where I left off.
+
+
+  private 
+
+    def tweet_params
+      params.require(:tweet).permit(:message)
+    end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def create 
+    @user = User.create(user_params)
+
+    if @user.save
+      render json: {
+        user: {
+          username: @user.username,
+          email: @user.email
+        }
+      }
+    else
+      render json: {
+        success: false
+      }
+    end 
+  end 
+
+  private 
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
